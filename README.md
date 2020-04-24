3e7d22bcfcf71b65320c9599f43359f97a5c50b4
ericlau00 <ericlau41802@gmail.com>
2020-04-24 10:47:25 -0400

    Reorganize repository structure

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # TwoFortyNine_jamalK-lauE-leeR
 
 ## Roster
+
 - Kazi Jamal: Project Manager
 - Eric Lau
 - Raymond Lee
---
+++ app/__init__.py
@@ -0,0 +1,6 @@
+'''
+TwoFortyNine -- Kazi Jamal, Eric Lau, and Raymond Lee
+SoftDev1 pd9
+P04 —- Let the Data Speak
+2020-05-04
+'''
