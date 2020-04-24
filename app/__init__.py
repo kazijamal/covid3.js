8f3c88960de4b55f15efd3643a559a1d62daabf4
ericlau00 <ericlau41802@gmail.com>
2020-04-24 10:58:19 -0400

    Initialize basic Flask app

--- app/__init__.py
+++ app/__init__.py
@@ -4,3 +4,19 @@ SoftDev1 pd9
 P04 —- Let the Data Speak
 2020-05-04
 '''
+
+from flask import Flask, request, redirect, session, render_template, url_for, flash
+import os
+
+app = Flask(__name__)
+app.secret_key = os.urandom(32)
+
+
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
