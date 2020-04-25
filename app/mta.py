d7a4c6a5a56b48ed45c77079c0788e4b6a47a2f1
ericlau00 <ericlau41802@gmail.com>
2020-04-25 08:08:52 -0400

    Implement time loop for MTA requests

--- app/mta.py
+++ app/mta.py
@@ -1,18 +1,38 @@
 import urllib.request
-import urllib.parse
+import datetime
 
 BASE_URL = 'http://web.mta.info/developers/data/nyct/turnstile/turnstile_'
-DATE = '200425'
 TXT = '.txt'
 
-req = urllib.request.Request(
-    BASE_URL + DATE + TXT
-)
+date = datetime.datetime(2016, 1, 2)
+last_date = datetime.datetime(2020, 4, 25)
 
-req = urllib.request.urlopen(req)
 
-res = req.read().decode('utf8').strip('\n').split('\n')
+def send_request(date):
+    req = urllib.request.Request(BASE_URL + date + TXT)
+    req = urllib.request.urlopen(req)
+    # print(req.status)
 
-res = [line.rstrip() for line in res]
+    res = req.read().decode('utf8').strip('\n').split('\n')
+    res = [line.rstrip() for line in res]
+    # print(res)
 
-print(res)
+start = datetime.datetime.now()
+
+while(date <= last_date):
+    # e.g. ['2020', '04', '25']
+    date_list = date.date().isoformat().split('-')
+
+    # e.g. '200425'
+    url_date = date_list[0][2:] + date_list[1] + date_list[2]
+
+    send_request(url_date)
+
+    date += datetime.timedelta(weeks=1)
+
+# 0:00:34:585861 to send all requests without reading
+# 0:00:09:131775 to send one request, read its content, and format it
+# There are 227 weeks from 2016-01-02 to 2020-04-25
+# It will take approximately 34 minutes to parse through all of the data
+
+print(datetime.datetime.now() - start)
