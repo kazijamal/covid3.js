7dc1ad9b7f30aa9f36a82ba0bbaac9437650e044
ericlau00 <ericlau41802@gmail.com>
2020-04-25 22:34:05 -0400

    Implement basic NYC map drawing

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,37 +1,38 @@
 window.onload = async () => {
     let svg = createSVG();
-    let boroughs = await getNYCBoroughs();
+    // let boroughs = await getNYCBoroughs();
+    let neighborhoods = await getNYCNeighborhoods();
 
-    console.log(boroughs);
     let projection = d3.geoMercator()
+                    .scale(50000)
   					.center([-73.94, 40.70])
-  					// .scale(50000)
-  					// .translate([(width) / 2, (height)/2]);
     let path = d3.geoPath(projection)
 
-    // console.log(svg);
     svg.selectAll('path')
-        .data(boroughs.features)
+        .data(neighborhoods)
         .join(
             enter => enter.append('path')
                 .attr('d', path)
                 .attr('fill', 'blue')
         );
-    console.log(svg);
 }
 
 let createSVG = () => {
     return d3.select('body').append('svg')
         .attr('id', 'map')
-        .attr('width', 975)
-        .attr('height', 610)
-        .attr('viewBox', [0, 0, 975, 610])
-        // .attr('width', '60%')
+        .attr('width', '75%')
+        .attr('viewBox', [73.94, -40.70, 975, 610])
         .append('g');
 }
 
 let getNYCBoroughs = async () => {
-    let boroughs = await d3.json('static/json/borough_boundaries.json');
+    let boroughs = await d3.json('static/json/boroughs.json');
 
-    return topojson.feature(boroughs, boroughs.objects.nyu_2451_34490);
+    return topojson.feature(boroughs, boroughs.objects.boroughs).features;
+}
+
+let getNYCNeighborhoods = async () => {
+    let neighborhoods = await d3.json('static/json/neighborhoods.json');
+
+    return neighborhoods.features;
 }
