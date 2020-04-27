790b8d5a94dbd6280e1323ac1fb5120ae77c4f00
ray. lee <rlee00@stuy.edu>
2020-04-27 13:43:41 -0400

    Init public-media branch

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,13 @@ app.secret_key = os.urandom(32)
 def root():
     return render_template('index.html')
 
+# absolute path to num-articles-per-day.csv
+num_articles_per_day_csv = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static", "csv", "num-articles-per-day.csv") 
+
+@app.route("/data/sentiment/publicmedia")
+def publicMediaData():
+    return open(num_articles_per_day_csv).read()    
+
 
 if __name__ == '__main__':
     app.debug = True
