5159952219ae1bc2934661113a25f8b9a6a08463
ericlau00 <ericlau41802@gmail.com>
2020-04-27 12:24:32 -0400

    Implement MTA data parsing algorithm

--- app/mta.py
+++ app/mta.py
@@ -1,26 +1,91 @@
 import urllib.request
 import datetime
+import time
+import pprint
 
-BASE_URL = 'http://web.mta.info/developers/data/nyct/turnstile/turnstile_'
-TXT = '.txt'
+BASE_URL, TXT = 'http://web.mta.info/developers/data/nyct/turnstile/turnstile_', '.txt'
 
-date = datetime.datetime(2016, 1, 2)
+date = datetime.datetime(2019, 1, 5)
 last_date = datetime.datetime(2020, 4, 25)
 
 
 def send_request(date):
     req = urllib.request.Request(BASE_URL + date + TXT)
     req = urllib.request.urlopen(req)
-    # print(req.status)
+    print(req.status)
+    res = req.read().decode('utf8').strip('\n').split('\n')
 
-    res = req.read().decode('utf8').strip('\n')
-        # .split('\n')
-    # res = [line.rstrip() for line in res]
-    # print(res)
+    # res = open('turnstile_200425.txt').read().strip('\n').split('\n')
+
+    res = [line.rstrip() for line in res][1:]
+
+    stations = dict()
+    station_names = list()
+
+    for i in range(0, len(res)):
+        event = res[i].split(',')
+
+        name, date = event[3], parse_date(event[6])
+        control, turnstile = event[0] + event[1], event[2]
+        _enter, _exit = int(event[9]), int(event[10])
+
+        if not name in stations:
+            stations[name] = dict()
+
+        _station = stations[name]
+        if not date in _station:
+            _station[date] = dict()
+
+        _date = _station[date]
+        if not control in _date:
+            _date[control] = dict()
+
+        _control = _date[control]
+        if not turnstile in _control:
+            _control[turnstile] = dict()
+            _turnstile = _control[turnstile]
+
+            _turnstile['enter'], _turnstile['exit'] = 0, 0
+
+            d_date = datetime.timedelta(days=1)
+            prev = date - d_date
+            if prev in _station and control in _station[prev] and turnstile in _station[prev][control]:
+                prev = _station[prev][control][turnstile]
+                prev['enter'] += _enter - prev['c_enter']
+                prev['exit'] += _exit - prev['c_exit']
+
+        else:
+            _turnstile = _control[turnstile]
+
+            _turnstile['enter'] += _enter - _turnstile['c_enter']
+            _turnstile['exit'] += _exit - _turnstile['c_exit']
+
+        _turnstile['c_enter'], _turnstile['c_exit'] = _enter, _exit
+
+    for station in stations.keys():
+        for date in stations[station]:
+            _date = stations[station][date]
+            day_enter, day_exit = 0, 0
+            keys = list(_date.keys())
+            for control in keys:
+                turnstiles = list(_date[control].keys())
+                for turnstile in turnstiles:
+                    day_enter += _date[control][turnstile]['enter']
+                    day_exit += _date[control][turnstile]['exit']
+                    _date[control].pop(turnstile)
+                _date.pop(control)
+            _date['enter'] = day_enter
+            _date['exit'] = day_exit
+
+    # pprint.pprint(stations)
 
 
-start = datetime.datetime.now()
+def parse_date(str_date):
+    date = [int(t) for t in str_date.split('/')]
+    return datetime.datetime(date[2], date[0], date[1])
 
+start = time.time()
+# count = 0
 # while(date <= last_date):
 #     # e.g. ['2020', '04', '25']
 #     date_list = date.date().isoformat().split('-')
@@ -30,13 +95,15 @@ start = datetime.datetime.now()
 
 #     send_request(url_date)
 
+#     count += 1
 #     date += datetime.timedelta(weeks=1)
 
-# # 0:00:34:585861 to send all requests without reading
+# # 0:00:12:674262 to send all requests without reading
 # # 0:00:09:131775 to send one request, read its content, and format it
-# # There are 227 weeks from 2016-01-02 to 2020-04-25
-# # It will take approximately 34 minutes to parse through all of the data
+# # There are 69 weeks from 2019-01-25 to 2020-04-25
+# # It will take approximately 10 minutes to parse through all of the data
+
 
 send_request('200425')
 
-print(datetime.datetime.now() - start)
+print(time.time() - start)
--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,20 +1,44 @@
 window.onload = async () => {
     let svg = createSVG();
-    // let boroughs = await getNYCBoroughs();
-    let neighborhoods = await getNYCNeighborhoods();
+    let boroughs = await getNYCBoroughs();
+    // let neighborhoods = await getNYCNeighborhoods();
+    let stops = await getSubwayStops();
 
     let projection = d3.geoMercator()
-                    .scale(50000)
-  					.center([-73.94, 40.70])
+        .scale(50000)
+        .center([-73.94, 40.70])
     let path = d3.geoPath(projection)
 
-    svg.selectAll('path')
-        .data(neighborhoods)
-        .join(
-            enter => enter.append('path')
-                .attr('d', path)
-                .attr('fill', 'blue')
-        );
+    let count = 0;
+
+    let station_names = Array()
+    for (const station in stops) {
+        let name = stops[station]['properties']['stop_name']
+        if(!station_names.includes(name)) {
+            count++;
+            station_names.push(name)
+        } else {
+            console.log(stops[station], station_names[station_names.indexOf(name)])
+        }
+        // console.log(stops[station]['properties']['stop_name'])
+    }
+    console.log(count)
+    // svg.selectAll('.subway-stop')
+    //     .data(stops)
+    //     .join(
+    //         enter => {
+    //             return enter.append('path')
+    //                 .attr('d', path)
+    //                 .attr('class', 'subway-stop')
+    //                 .attr('fill', d => {
+    //                     count++;
+    //                     console.log(d.properties.stop_name, d.properties.trains)
+    //                     return 'red'
+    //                 })
+    //         }
+    //     );
+
+    console.log(count);
 }
 
 let createSVG = () => {
@@ -35,4 +59,12 @@ let getNYCNeighborhoods = async () => {
     let neighborhoods = await d3.json('static/json/neighborhoods.json');
 
     return neighborhoods.features;
+}
+
+let getSubwayStops = async () => {
+    let stops = await d3.json('static/json/subway_stops.json')
+
+    // console.log(stops)
+    return topojson.feature(stops, stops.objects.subway_stops).features;
+
 }
