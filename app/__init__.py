385cda498549713cf711f5be133b226d3256a860
Kazi Jamal <jamalkazi02@gmail.com>
2020-04-28 02:40:43 -0400

    Add routes and templates for viewable pages

--- app/__init__.py
+++ app/__init__.py
@@ -11,11 +11,45 @@ import os
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
+# DASHBOARD
 
 @app.route('/')
 def root():
-    return render_template('index.html')
+    return render_template('dashboard.html')
 
+# SENTIMENT ANALYSIS
+
+@app.route('/sentiment')
+def sentiment():
+    return render_template('sentiment/sentiment.html')
+
+@app.route('/sentiment/publicmedia')
+def publicmedia():
+    return render_template('sentiment/publicmedia.html')
+
+@app.route('/sentiment/trumptweets')
+def trumptweets():
+    return render_template('sentiment/trumptweets.html')
+
+# TRANSPORTATION
+
+@app.route('/transportation')
+def transportation():
+    return render_template('transportation/transportation.html')
+
+@app.route('/transportation/nycpublic')
+def nycpublic():
+    return render_template('transportation/nycpublic.html')
+
+@app.route('/transportation/mobility')
+def mobility():
+    return render_template('transportation/mobility.html')
+
+# NUMBERS
+
+@app.route('/numbers')
+def numbers():
+    return render_template('numbers.html')
 
 if __name__ == '__main__':
     app.debug = True
