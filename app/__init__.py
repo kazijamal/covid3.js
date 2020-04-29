e32f0230c7fe74a59a6d316ee7ba264636d141be
Kazi Jamal <jamalkazi02@gmail.com>
2020-04-28 22:04:16 -0400

    Create about page and route

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,11 @@ app.secret_key = os.urandom(32)
 def root():
     return render_template('dashboard.html')
 
+# ABOUT
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 # SENTIMENT ANALYSIS
 @app.route('/sentiment')
 def sentiment():
