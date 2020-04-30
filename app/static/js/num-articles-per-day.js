050f2fbbae818d096447c9fe7a91676e345702f7
ray. lee <rlee00@stuy.edu>
2020-04-29 20:02:06 -0400

    Create month_day labels for x-axis

--- app/static/js/num-articles-per-day.js
+++ app/static/js/num-articles-per-day.js
@@ -11,19 +11,12 @@ const numDatapoints = 104;
 // X scale maps the index of our data to the width of the graph
 const xScale = d3
   .scaleLinear()
-  .domain([0, numDatapoints - 1])
+  // .domain([0, numDatapoints - 1])
   .range([0, width]);
 
 // Y scale maps the upper bound of the number of articles (2000) to the height of the graph
 // TODO: extract upper bound dynamically
 const yScale = d3.scaleLinear().domain([0, 2000]).range([height, 0]);
-
-// d3's line generator
-const line = d3
-  .line()
-  .x((_, i) => xScale(i)) // Set x values for the line generator
-  .y((d) => yScale(d.y)) // Set y values for the line generator
-  .curve(d3.curveMonotoneX); // Apply smoothing to the curve
 
 // Append the SVG object to the body of the page
 const svg = d3
@@ -36,11 +29,33 @@ const svg = d3
 
 const numArticlesPerDay = {};
 
+const months = {
+  0: 'January',
+  1: 'February',
+  2: 'March',
+  3: 'April',
+  4: 'May',
+  5: 'June',
+  6: 'July',
+  7: 'August',
+  8: 'September',
+  9: 'October',
+  10: 'November',
+  11: 'December',
+};
+
 // Read the data
 // TODO: Split this chunk into smaller, intentional pieces
 d3.csv('/data/sentiment/publicmedia')
   .then((numArticles) => {
-    // xScale.domain(d3.extent(numArticles, (d) => d.date));
+    xScale.domain(d3.extent(numArticles, (d) => new Date(d.date)));
+
+    // d3's line generator
+    const line = d3
+      .line()
+      .x((d) => xScale(new Date(d.date))) // Set x values for the line generator
+      .y((d) => yScale(d.y)) // Set y values for the line generator
+      .curve(d3.curveMonotoneX); // Apply smoothing to the curve
 
     // yScale.domain(d3.extent([0, d3.max(numArticles, (d) => +d.numArticles)]));
 
@@ -48,19 +63,34 @@ d3.csv('/data/sentiment/publicmedia')
       dayData = numArticles[day];
       numArticlesPerDay[day] = { ...dayData, date: new Date(dayData.date) };
     }
+
     const numArticlesPerDayData = d3
       .range(numDatapoints)
       .map((d) => ({ y: +numArticlesPerDay[d].numArticles }));
+
+    for (const key in numArticles) {
+      numArticlesPerDayData[new Date(numArticles[key].date)] =
+        numArticlesPerDayData[key];
+    }
+
+    console.log(numArticlesPerDayData);
 
     // Call the x-axis in a group tag
     svg
       .append('g')
       .attr('class', 'x-axis')
       .attr('transform', `translate(0, ${height})`)
-      .call(d3.axisBottom(xScale)); // Create an x-axis component with d3.axisBottom
+      .call(
+        d3.axisBottom(xScale).tickFormat((date) => {
+          const dateObj = new Date(date);
+          return `${months[dateObj.getMonth()]} ${dateObj.getDate()}`;
+        })
+      ); // Create an x-axis component with d3.axisBottom
 
     // Call the y-axis in a group tag
     svg.append('g').attr('class', 'y-axis').call(d3.axisLeft(yScale)); // Create a y-axis component with d3.axisLeft
+
+    console.log(numArticlesPerDayData);
 
     // Append the path, bind the data, and call the line generator
     svg
@@ -76,7 +106,12 @@ d3.csv('/data/sentiment/publicmedia')
       .enter()
       .append('circle')
       .attr('class', 'dot')
-      .attr('cx', (_, i) => xScale(i))
+      .attr('cx', (_, i) => {
+        const { date } = numArticlesPerDay[i];
+        // console.log(`${date.getMonth() + 1}/${date.getDate()}`);
+        // console.log(xScale(new Date(date)));
+        return xScale(new Date(date));
+      })
       .attr('cy', (d) => yScale(d.y))
       .attr('r', 5);
     // TODO: Add on mouseover events on these datapoint circles
