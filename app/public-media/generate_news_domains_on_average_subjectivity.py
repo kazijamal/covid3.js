6ce552174efd7cbd1a00fecb9186ac712a1f3de6
ray. lee <rlee00@stuy.edu>
2020-05-06 10:18:08 -0400

    Upload generation scripts

---
+++ app/public-media/generate_news_domains_on_average_subjectivity.py
@@ -0,0 +1,29 @@
+import csv
+import sys
+from textblob import TextBlob
+
+csv.field_size_limit(sys.maxsize) # Resolves error: _csv.Error: field larger than field limit (131072)
+
+sentiment_dict = {}
+
+filename = "covid19_articles.csv"
+counter = 0
+with open(filename) as csvfile:
+  articles = csv.reader(csvfile, delimiter=",")
+  next(articles)
+  for article in articles:
+    domain = article[4]
+    if domain in sentiment_dict:
+      sentiment_dict[domain] += float(TextBlob(article[6]).sentiment.subjectivity)
+    else:
+      sentiment_dict[domain] = float(TextBlob(article[6]).sentiment.subjectivity)
+
+average_polarity_dict = {}
+for key, val in sentiment_dict.items():
+  average_polarity_dict[key] = val / domains[key]
+
+with open("output.csv", "w") as output_file:
+  output = csv.writer(output_file, delimiter=",")
+
+  for key, val in average_polarity_dict.items():
+    output.writerow([key, val])
