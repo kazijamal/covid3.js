64e0187cb667e81c132093ea3427d24f7373840c
ray. lee <rlee00@stuy.edu>
2020-05-07 19:29:37 -0400

    Apply color interpolator to dots

--- app/static/js/trumptweets/trump-tweets-on-polarity-range.js
+++ app/static/js/trumptweets/trump-tweets-on-polarity-range.js
@@ -2,7 +2,7 @@
 
 // Set the dimensions and margins of the graph
 const margin = { top: 50, right: 50, bottom: 50, left: 50 };
-const width = window.innerWidth - margin.left - margin.right; // Use window's width
+const width = 800; // Use window's width
 const height = window.innerHeight - margin.top - margin.bottom; // Use window's height
 
 // TODO: extract numDatapoints dynamically
@@ -22,19 +22,24 @@ const yScale = d3.scaleLinear().domain([-1, 1]).range([height, 0]);
 const line = d3
   .line()
   .x((_, i) => xScale(i)) // Set x values for the line generator
-  .y((d) => yScale(d.y)) // Set y values for the line generator
+  .y((d) => yScale(d.polarity)) // Set y values for the line generator
   .curve(d3.curveMonotoneX); // Apply smoothing to the curve
 
 // Append the SVG object to the body of the page
 const svg = d3
   .select('#scatterplot')
   .append('svg')
-  .attr('width', width + margin.left + margin.right)
+  .attr('width', 900)
   .attr('height', height + margin.top + margin.bottom)
   .append('g')
   .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 const numArticlesPerDay = {};
+
+const dotColorInterpolator = d3
+  .scaleSequential()
+  .domain([-1.0, 1.0])
+  .interpolator(d3.interpolateViridis);
 
 // Read the data
 // TODO: Split this chunk into smaller, intentional pieces
@@ -45,14 +50,14 @@ d3.csv('/data/sentiment/trumptweetspolarities')
     }
     const numArticlesPerDayData = d3
       .range(numDatapoints)
-      .map((d) => ({ y: +numArticlesPerDay[d].polarity }));
+      .map((d) => ({ polarity: +numArticlesPerDay[d].polarity }));
 
     // Call the x-axis in a group tag
     svg
       .append('g')
       .attr('class', 'x-axis')
       .attr('transform', `translate(0, ${height})`)
-      .call(d3.axisBottom(xScale)); // Create an x-axis component with d3.axisBottom
+      .call(d3.axisBottom(xScale).ticks(0)); // Create an x-axis component with d3.axisBottom
 
     // Call the y-axis in a group tag
     svg.append('g').attr('class', 'y-axis').call(d3.axisLeft(yScale)); // Create a y-axis component with d3.axisLeft
@@ -72,8 +77,12 @@ d3.csv('/data/sentiment/trumptweetspolarities')
       .append('circle')
       .attr('class', 'dot')
       .attr('cx', (_, i) => xScale(i))
-      .attr('cy', (d) => yScale(d.y))
-      .attr('r', 5);
+      .attr('cy', (d) => yScale(d.polarity))
+      .attr('r', 5)
+      .attr('fill', (d) => {
+        console.log(d);
+        return dotColorInterpolator(d.polarity);
+      });
     // TODO: Add on mouseover events on these datapoint circles
   })
   .catch((err) => console.log(err));
