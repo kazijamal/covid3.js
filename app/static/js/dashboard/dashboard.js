e641506feae5b9e39fe9ee5eeae2965deb5bd671
ericlau00 <ericlau41802@gmail.com>
2020-05-08 11:01:12 -0400

    Add legend as group to line graph

--- app/static/js/dashboard/dashboard.js
+++ app/static/js/dashboard/dashboard.js
@@ -28,17 +28,12 @@ let uslinegraph = async () => {
         deaths["values"].push(+data[i]["deaths"]);
     }
 
-    let colorMap = {
-        cases: "#2315ba",
-        deaths: "#911111",
-    };
-
     let svg = d3
         .select("#us-line-container")
         .append("svg")
         .attr("id", "us-line-graph")
         .attr("width", "100%")
-        .attr("height", "60vh");
+        .attr("height", "50vh");
 
     let margin = { top: 50, right: 50, bottom: 50, left: 100 };
 
@@ -51,21 +46,45 @@ let uslinegraph = async () => {
         margin,
         "us-line",
         "date-x",
-		"people-y",
-		{strokewidth: 4}
+        "people-y",
+        { strokewidth: 4 }
     );
 
     await usline.renderMultiLine([minDate, maxDate], {
         cases: "#2315ba",
         deaths: "#911111",
-	});
-	
+    });
+
     usline.yLabel('# of people');
+
+    let legend = svg
+        .append('g')
+        .attr('transform', 'translate(100, 100)')
+
+    legend.append("circle")
+        .attr("cx", 70)
+        .attr("cy", 30)
+        .attr("r", 6)
+        .style("fill", "#2315ba")
+
+    legend.append("text")
+        .attr("x", 90)
+        .attr("y", 30)
+        .text("cases")
+        .style("font-size", "15px")
+        .attr("alignment-baseline", "middle")
+
+
+    legend.append("circle")
+        .attr("cx", 70)
+        .attr("cy", 60)
+        .attr("r", 6)
+        .style("fill", "#911111")
+
+    legend.append("text")
+        .attr("x", 90)
+        .attr("y", 60)
+        .text("deaths")
+        .style("font-size", "15px")
+        .attr("alignment-baseline", "middle")
 };
-
-var svg = d3.select("#us-line-legend")
-
-svg.append("circle").attr("cx",70).attr("cy",30).attr("r", 6).style("fill", "#2315ba")
-svg.append("circle").attr("cx",70).attr("cy",60).attr("r", 6).style("fill", "#911111")
-svg.append("text").attr("x", 90).attr("y", 30).text("cases").style("font-size", "15px").attr("alignment-baseline","middle")
-svg.append("text").attr("x", 90).attr("y", 60).text("deaths").style("font-size", "15px").attr("alignment-baseline","middle")
